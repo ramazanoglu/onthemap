f654ed7dff53b6cdc697d532d942ed28f6c9e8db
Gokturk Ramazanoglu <gokturk@zigzag.is>
2017-11-22 18:20:50 +0100

    Added new parse methods

--- OnTheMap/Model/Clients/ParseClient.swift
+++ OnTheMap/Model/Clients/ParseClient.swift
@@ -10,31 +10,81 @@ import Foundation
 
 class ParseClient : NSObject {
     
-    
-    
     func getStudentLocations(completionHandler: @escaping(_ result: String?, _ error: String?) -> Void) {
         
-        var request = URLRequest(url: URL(string: "https://parse.udacity.com/parse/classes/StudentLocation")!)
-        request.addValue("QrX47CA9cyuGewLdsL7o5Eb8iug6Em8ye0dnAbIr", forHTTPHeaderField: "X-Parse-Application-Id")
-        request.addValue("QuWThTdiRmTux3YaDseUSEpUKo7aBYM737yKd4gY", forHTTPHeaderField: "X-Parse-REST-API-Key")
+        func sendError(_ error: String) {
+            print(error)
+            completionHandler(nil, error)
+        }
+        
+        let queryItems : [String : AnyObject] = ["limit" : "5" as AnyObject,
+                                                 "order" : "-updatedAt" as AnyObject]
+        
+        let url = createURLComponents(path: "/parse/classes/StudentLocation", queryItems: queryItems)
+        
+        let request = createURLRequest(url: url)
         let session = URLSession.shared
         let task = session.dataTask(with: request) { data, response, error in
-            if error != nil { // Handle error...
+            
+            if error != nil {
+                sendError("There was an error with your request: \(error!)")
                 return
             }
-            print(String(data: data!, encoding: .utf8)!)
             
-            completionHandler(String(data: data!, encoding: .utf8)!, nil)
+            guard let statusCode = (response as? HTTPURLResponse)?.statusCode, statusCode >= 200 && statusCode <= 299 else {
+                sendError("Status Code")
+                return
+            }
+            
+            guard let data = data else {
+                sendError("No data was returned by the request!")
+                return
+            }
+            
+            var parsedResult: [String:AnyObject]!
+            
+            do {
+                parsedResult = try JSONSerialization.jsonObject(with: data, options: .allowFragments) as! [String:AnyObject]
+                
+            } catch {
+                sendError("There is a problem with connection")
+                return
+            }
+            
+            let results = parsedResult["results"] as! [[String:AnyObject]]
+            
+            for result in results {
+                
+                let studentInformation = StudentLocation.init(dictionary : result)
+                
+                print("\(studentInformation)")
+                
+            }
+            
+            print("*************************************")
+            print(parsedResult)
+            
+            //            completionHandler(String(data: data!, encoding: .utf8)!, nil)
         }
         task.resume()
-
+        
     }
     
     func getStudentLocation(uniqueKey: String, completionHandler: @escaping(_ result: String?, _ error: String?) -> Void) {
         
-        let urlString = "https://parse.udacity.com/parse/classes/StudentLocation"
-        let url = URL(string: urlString)
-        var request = URLRequest(url: url!)
+        var components = URLComponents()
+        components.scheme = "https"
+        components.host = "parse.udacity.com"
+        components.path = "/parse/classes/StudentLocation"
+        components.queryItems = [URLQueryItem]()
+        
+        let queryItem1 = URLQueryItem(name: "where", value: "{\"uniqueKey\":\"\(uniqueKey)\"}")
+        
+        components.queryItems!.append(queryItem1)
+        
+        print("\(components.url)")
+        
+        var request = URLRequest(url: components.url!)
         request.addValue("QrX47CA9cyuGewLdsL7o5Eb8iug6Em8ye0dnAbIr", forHTTPHeaderField: "X-Parse-Application-Id")
         request.addValue("QuWThTdiRmTux3YaDseUSEpUKo7aBYM737yKd4gY", forHTTPHeaderField: "X-Parse-REST-API-Key")
         let session = URLSession.shared
@@ -45,12 +95,34 @@ class ParseClient : NSObject {
             print(String(data: data!, encoding: .utf8)!)
         }
         task.resume()
-
-        
-        
         
     }
     
+    
+    func createURLRequest(url :URL) -> URLRequest {
+        var request = URLRequest(url: url)
+        request.addValue("QrX47CA9cyuGewLdsL7o5Eb8iug6Em8ye0dnAbIr", forHTTPHeaderField: "X-Parse-Application-Id")
+        request.addValue("QuWThTdiRmTux3YaDseUSEpUKo7aBYM737yKd4gY", forHTTPHeaderField: "X-Parse-REST-API-Key")
+        
+        return request
+    }
+    
+    func createURLComponents(path: String, queryItems: [String: AnyObject])  -> URL{
+        
+        var components = URLComponents()
+        components.scheme = "https"
+        components.host = "parse.udacity.com"
+        components.path = path
+        components.queryItems = [URLQueryItem]()
+        
+        for queryItem in queryItems {
+            
+            components.queryItems?.append(URLQueryItem(name: queryItem.key, value: "\(queryItem.value)"))
+            
+        }
+        return components.url!
+        
+    }
     
     class func sharedInstance() -> ParseClient {
         struct Singleton {
---
+++ OnTheMap/Model/StudentLocation.swift
@@ -0,0 +1,39 @@
+//
+//  StudentLocation.swift
+//  OnTheMap
+//
+//  Created by Gokturk Ramazanoglu on 22.11.17.
+//  Copyright © 2017 udacity. All rights reserved.
+//
+
+import Foundation
+
+struct StudentLocation  {
+    
+    var createdAt:String?
+    var firstName:String?
+    var lastName:String?
+    var latitude:Double?
+    var longitude:Double?
+    var mapString:String?
+    var mediaUrl:String?
+    var objectID:String?
+    var uniqueKey:String?
+    var updatedAt:String?
+    
+    
+    init(dictionary : [String: AnyObject]) {
+        self.createdAt = dictionary["createdAt"] as? String
+        self.firstName = dictionary["firstName"] as? String
+        self.lastName = dictionary["lastName"] as? String
+        self.latitude = dictionary["latitude"] as? Double
+        self.longitude = dictionary["longitude"] as? Double
+        self.mapString = dictionary["mapString"] as? String
+        self.mediaUrl = dictionary["mediaURL"] as? String
+        self.objectID = dictionary["objectId"] as? String
+        self.uniqueKey = dictionary["uniqueKey"] as? String
+        self.updatedAt = dictionary["updatedAt"] as? String
+        
+    }
+    
+}
